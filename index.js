142787b668ec8d5c20207cb14c21cee0d87fffc3
sdcro <sdcrouse91@gmail.com>
2020-06-09 00:05:53 -0700

    Get first Concoction by default when page is loaded
    Note: As mentioned in the code comment, this default behavior will be changed later.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const BASE_URL = "http://localhost:3000/api/v1/concoctions";
+
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("Page loaded successfully!");
-  console.log("Welcome to the Crazy Coffee Concoctions app!");
+  getConcoction(1); // This is a temporary default, until I add the "New Concoction" form.
 });
+
+function getConcoction(concoction_id) {
+  fetch(`${BASE_URL}/1`)
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+}
