da0c6c2e1341286f928e0ae24eddf93712e72677
sdcro <sdcrouse91@gmail.com>
2020-06-21 19:00:11 -0700

    Add Coffee class

---
+++ src/coffee.js
@@ -0,0 +1,8 @@
+class Coffee {
+  constructor(id, coffeeAttributes) {
+    this.id = id;
+    this.amount = coffeeAttributes.amount;
+    this.brand = coffeeAttributes.brand;
+    this.variety = coffeeAttributes.variety;
+  }
+}
