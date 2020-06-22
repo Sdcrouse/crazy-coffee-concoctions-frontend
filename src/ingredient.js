7e003fba2feff0fc083351434d910df99ab84574
sdcro <sdcrouse91@gmail.com>
2020-06-21 19:02:51 -0700

    Add Ingredient class

---
+++ src/ingredient.js
@@ -0,0 +1,8 @@
+class Ingredient {
+  constructor(id, ingredientAttributes) {
+    this.id = id;
+    this.amount = ingredientAttributes.amount;
+    this.name = ingredientAttributes.name;
+    this.category = ingredientAttributes.category;
+  }
+}
