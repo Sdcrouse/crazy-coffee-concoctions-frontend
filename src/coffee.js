6b8135554595285a249a24e41706fa36fef5f45a
sdcro <sdcrouse91@gmail.com>
2020-06-22 21:09:15 -0700

    Refactor Coffee static method with a Coffee instance method
    The Coffee static method appendCoffeeList originally had a function that operated on a Coffee object.
    That function is now an instance method of the Coffee class, as it should be.

--- src/coffee.js
+++ src/coffee.js
@@ -6,25 +6,25 @@ class Coffee {
     this.variety = coffeeAttributes.variety;
   }
 
+  appendCoffeeItemTo(coffeeList) {
+    const coffeeItem = document.createElement('li');
+    coffeeItem.textContent = this.amount; // Be sure to refactor this with something like newElementWithText.
+
+    if(this.brand) {
+      coffeeItem.textContent += ` ${this.brand}`;
+    }
+
+    coffeeItem.textContent += ` ${this.variety}`;
+    coffeeList.append(coffeeItem);
+  }
+
   static appendCoffeeList(coffeeObjs, wrapper) {
     // Create a list of coffees, and append it to the wrapper element.
     const coffeeLabel = document.createElement('h3');
     const coffeeList = document.createElement('ul');
 
     coffeeLabel.textContent = "Coffee(s):";
-
-    coffeeObjs.forEach(function (coffeeObj) {
-      const coffeeItem = document.createElement('li');
-      coffeeItem.textContent = `${coffeeObj.amount} `;
-
-      if(coffeeObj.brand) {
-        coffeeItem.textContent += `${coffeeObj.brand} `;
-      }
-
-      coffeeItem.textContent += `${coffeeObj.variety}`;
-      coffeeList.append(coffeeItem);
-    });    
-
+    coffeeObjs.forEach(coffeeObj => coffeeObj.appendCoffeeItemTo(coffeeList));
     wrapper.append(coffeeLabel, coffeeList);
   }
 }
