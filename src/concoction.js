8923277ec9b8a8c67045db1cf394d4c60d941f56
sdcro <sdcrouse91@gmail.com>
2020-06-22 17:47:48 -0700

    Refactor Concoction class with more DRY code
    As noted in the helper method, this is more DRY, but not as easily extensible.
    I may change this in a later version, if the app's JSON structure ever changes.

--- src/concoction.js
+++ src/concoction.js
@@ -4,14 +4,16 @@ class Concoction {
     this.name = concoctionAttributes.name;
     this.instructions = concoctionAttributes.instructions;
     this.notes = concoctionAttributes.notes;
-    this.coffees = this.createCollection(includedObjects, 'coffee', Coffee);
-    this.ingredients = this.createCollection(includedObjects, 'ingredient', Ingredient);
+    this.coffees = this.createCollection(includedObjects, Coffee);
+    this.ingredients = this.createCollection(includedObjects, Ingredient);
 
     Concoction.all.push(this);
   }
 
-  createCollection(objects, objType, objClass) {
-    const filtered = objects.filter(obj => obj.type === objType);
+  createCollection(objects, objClass) {
+    // This is DRY, but it may be hard to extend later.
+    // It assumes that the object's type is its lowercased class name.
+    const filtered = objects.filter(obj => obj.type === objClass.name.toLowerCase());
     return filtered.map(obj => new objClass(obj.id, obj.attributes));
   }
 }
