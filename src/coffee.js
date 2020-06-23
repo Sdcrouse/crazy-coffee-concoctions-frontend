3c080f6c5c8dff4fc5b1f6260ad5b13333dba2d3
sdcro <sdcrouse91@gmail.com>
2020-06-22 20:55:55 -0700

    Add static Coffee method that appends a list of coffees to a wrapper

--- src/coffee.js
+++ src/coffee.js
@@ -5,4 +5,26 @@ class Coffee {
     this.brand = coffeeAttributes.brand;
     this.variety = coffeeAttributes.variety;
   }
+
+  static appendCoffeeList(coffeeObjs, wrapper) {
+    // Create a list of coffees, and append it to the wrapper element.
+    const coffeeLabel = document.createElement('h3');
+    const coffeeList = document.createElement('ul');
+
+    coffeeLabel.textContent = "Coffee(s):";
+
+    coffeeObjs.forEach(function (coffeeObj) {
+      const coffeeItem = document.createElement('li');
+      coffeeItem.textContent = `${coffeeObj.amount} `;
+
+      if(coffeeObj.brand) {
+        coffeeItem.textContent += `${coffeeObj.brand} `;
+      }
+
+      coffeeItem.textContent += `${coffeeObj.variety}`;
+      coffeeList.append(coffeeItem);
+    });    
+
+    wrapper.append(coffeeLabel, coffeeList);
+  }
 }
