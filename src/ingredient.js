3e35c364c6ac8bbe0e09c8a102a83288c046f8c2
sdcro <sdcrouse91@gmail.com>
2020-06-23 18:18:06 -0700

    Add an Ingredient method that (temporarily) appends only creamers to a wrapper
    Right now, Ingredient.appendIngredientList only appends creamers to the displayed Concoction,
    but it will ultimately append ALL of a Concoction's ingredients (sorted by category).
    The goal here is to refactor index.js with the Ingredient class.

--- src/ingredient.js
+++ src/ingredient.js
@@ -5,6 +5,23 @@ class Ingredient {
     this.name = ingredientAttributes.name;
     this.category = ingredientAttributes.category;
   }
+
+  static appendIngredientList(ingredients, wrapper) {
+    const creamers = ingredients.filter(ingred => ingred.category === "creamer");
+    
+    if(creamers.length > 0) {
+      // This and the Concoction class's appendLabeledAttribute method might be refactored with the App class.
+      const label = App.newElementWithText('h3', `Creamer(s):`);
+      const creamerList = document.createElement('ul');
+      
+      creamers.forEach(creamer => {
+        const creamerItem = App.newElementWithText('li', `${creamer.amount} ${creamer.name}`);
+        creamerList.append(creamerItem);
+      });
+
+      wrapper.append(label, creamerList);
+    }
+  }
 }
 
 Ingredient.allCategories = ["Liquid", "Sweetener", "Creamer", "Other"];
