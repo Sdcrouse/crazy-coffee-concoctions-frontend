080578068a3aea980d17c5feb9d650f9c3b7930d
sdcro <sdcrouse91@gmail.com>
2020-06-26 20:32:16 -0700

    Refactor Ingredient class with Shared.labeledCollectionList

--- src/ingredient.js
+++ src/ingredient.js
@@ -6,16 +6,6 @@ class Ingredient {
     this.category = ingredientAttributes.category;
   }
 
-  static ingredientList(ingredients) {
-    const list = document.createElement('ul');
-
-    ingredients.forEach(
-      ingred => list.append( Shared.newElementWithText('li', `${ingred.amount} ${ingred.name}`) )
-    );
-
-    return list;
-  }
-
   static categoryLabel(category) {
     return (category === "Other" ? "Additional Ingredient(s):" : `${category}(s):`);
   }
@@ -29,10 +19,9 @@ class Ingredient {
       const filteredByCategory = ingredients.filter(ingred => ingred.category === category.toLowerCase());
       
       if(filteredByCategory.length > 0) { // I.e. there are ingredients with this category
-        return [
-          Shared.newElementWithText('h3', this.categoryLabel(category)),
-          this.ingredientList(filteredByCategory)
-        ];
+        return Shared.labeledCollectionList(
+          this.categoryLabel(category), filteredByCategory, (ingredient) => `${ingredient.amount} ${ingredient.name}`
+        );
       }
     }).filter(element => !!element); // Filter out any falsy (usually undefined) values
   } // End of labeledIngredientLists
