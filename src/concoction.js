9af75bd3e01810609504263df43f7166bdc32c05
sdcro <sdcrouse91@gmail.com>
2020-07-10 16:48:35 -0700

    Remove old Concoction method

--- src/concoction.js
+++ src/concoction.js
@@ -10,11 +10,6 @@ class Concoction {
     Concoction.all.push(this);
   }
 
-  createCollection(objects, objClass, objType = objClass.name.toLowerCase()) {
-    const filtered = objects.filter(obj => obj.type === objType);
-    return filtered.map(obj => new objClass(obj.id, obj.attributes));
-  }
-
   nameWrapper() {
     const wrapper = document.createElement('div');
     const nameHeading = Shared.newElementWithText('h2', this.name);
