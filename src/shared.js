84bb5b89adf51f2974d3846f6f07a28e7406c173
sdcro <sdcrouse91@gmail.com>
2020-07-13 12:59:05 -0700

    Remove unused Shared class
    The Shared class, as of this time, only has the method newElementWithText.
    That method has been replaced with the createNewElementWithText function in index.js.
    Since no files use the Shared class anymore, it is no longer needed.

--- src/shared.js
+++
@@ -1,8 +0,0 @@
-class Shared {
-  static newElementWithText(elementType, elementText) {
-    const newElement = document.createElement(elementType);
-
-    newElement.textContent = elementText;
-    return newElement;
-  }
-}
